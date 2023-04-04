13adf73180de5669ee05190ef28a1ec631739e1f
Anasks19 <anasks1999@gmail.com>
2023-04-04 21:54:09 +0000

    double pointer

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: double pointer
+ * @to: pointer
+ *
+ * Return: Nothing
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+return;
+}
