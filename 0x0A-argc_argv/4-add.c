ee595bfed2dfcd4f4bdace107a44e4f713ac15a0
Anasks19 <anasks1999@gmail.com>
2023-04-09 00:15:37 +0000

    adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,36 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /**
- * main - Entry point
+ * main - entry point
  * @argc: argument count
  * @argv: argument vector
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 (Success).
  */
 
 int main(int argc, char *argv[])
 {
 int count = 1;
 int sum = 0;
+int i;
 
-if (*argv[count] >= 48 && *argv[count] <= 57 && argc > 2) 
+if (argc > 2)
 {
 while (count < argc)
 {
-if ((*argv[count] >= 33 && *argv[count] <= 47) || (*argv[count] >= 97 && 
-*argv[count] <= 122) || (*argv[count] >= 65 && *argv[count] <= 90))
+i = 0;
+if (atoi(argv[count]) < 0 || !isdigit(argv[count][i]))
 {
 printf("Error\n");
 return (1);
-break;
 }
+i++;
 sum += atoi(argv[count]);
 count++;
 }
 printf("%d\n", sum);
 }
 else
+{
 printf("0\n");
+}
 return (0);
 }
