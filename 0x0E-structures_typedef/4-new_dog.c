21d00a7e472b94d2879f8d6be01eaf8945a32766
Anasks19 <anasks1999@gmail.com>
2023-04-17 23:18:21 +0000

    task 5

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,28 +1,57 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+#include <stdlib.h>
 
 /**
- * main - check the code
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
  *
- * Return: Always 0.
+ * Return: pointer to the new dog
  */
-int main(void)
-{
-dog_t *my_dog;
 
-my_dog = new_dog("Poppy", 3.5, "Bob");
-if (my_dog == NULL)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-printf("Failed to create new dog\n");
-return (1);
+dog_t *dog;
+char *name_copy, *owner_copy;
+int name_len = 0, owner_len = 0;
+
+dog = malloc(sizeof(dog_t));
+if (dog == NULL)
+return (NULL);
+
+/* compute length of name and owner */
+while (name[name_len] != '\0')
+name_len++;
+while (owner[owner_len] != '\0')
+owner_len++;
+
+/* allocate memory for name and owner copies */
+name_copy = malloc(sizeof(char) * (name_len + 1));
+if (name_copy == NULL)
+{
+free(dog);
+return (NULL);
 }
-printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
+owner_copy = malloc(sizeof(char) * (owner_len + 1));
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(dog);
+return (NULL);
+}
 
-/* don't forget to free the memory */
-free(my_dog->name);
-free(my_dog->owner);
-free(my_dog);
+/* copy name and owner to their respective copies */
+for (int i = 0; i <= name_len; i++)
+name_copy[i] = name[i];
+for (int i = 0; i <= owner_len; i++)
+owner_copy[i] = owner[i];
 
-return (0);
+/* assign values to dog elements */
+dog->name = name_copy;
+dog->age = age;
+dog->owner = owner_copy;
+
+return (dog);
 }
