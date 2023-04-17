3f1833ff896603d5298b5e15637f599c0ecf58d7
Anasks19 <anasks1999@gmail.com>
2023-04-17 18:00:18 +0000

    File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+printf("%s\n", __FILE__);
+
+return (EXIT_SUCCESS);
+}
