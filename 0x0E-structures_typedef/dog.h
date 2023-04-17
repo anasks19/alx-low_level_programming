e7bf29a0f93e86138695c4cd7993283f9079ee83
Anasks19 <anasks1999@gmail.com>
2023-04-17 22:36:13 +0000

    task1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - declare struct
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ * Description: Struct containing dog's name, age and owner.
+ */
+
 struct dog
 {
 char *name;
